887fa04f498882478ab9ec000713ba692f21067d
Natsuki Yamaguchi <natsuki.yamaguchi.62@gmail.com>
2020-12-02 08:21:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/Products' component={Products}/>
+        <Route path='/Reports' component={Reports}/>
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
